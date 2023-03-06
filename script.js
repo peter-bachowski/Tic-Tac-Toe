5f0249632d1f869644787ea75f4c1f09651be9a8
Peter <bachowskip@gmail.com>
2023-03-05 22:23:02 -0800

    Add a lightbox and form

--- script.js
+++ script.js
@@ -1,11 +1,31 @@
 const gridContainer = document.getElementById("gridContainer");
 const gridCell = document.createElement("div");
+const startBtn = document.getElementById("startBtn");
+const lightbox = document.getElementById("lightbox");
+
+const player = (name, symbol) => {
+    return {name, symbol};
+};
 
 let cellArray = [];
 
 gridCell.classList.add("gridCell");
 
 for (let cellCount = 0; cellCount < 9; cellCount++){
-    cellArray[cellCount] = gridCell.cloneNode(true);
-    gridContainer.appendChild(cellArray[cellCount]);
+    let cell = cellArray[cellCount];
+    cell = gridCell.cloneNode(true);
+    gridContainer.appendChild(cell);
+    cell.addEventListener("click", cellClickFunc(cell));
+}
+
+function cellClickFunc (el) {
+    
+}
+
+startBtn.onclick = () => {
+    lightbox.className = "on";
+}
+
+lightbox.onclick = () => {
+    lightbox.className = "off";
 }
