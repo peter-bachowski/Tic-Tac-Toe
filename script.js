ce85e2a663c6d6ce4fe0248e96888ad9b9a082ae
Replit user <>
2023-03-07 01:49:01 +0000

    Edit some functions in js file, fix cell array

--- script.js
+++ script.js
@@ -2,30 +2,35 @@ const gridContainer = document.getElementById("gridContainer");
 const gridCell = document.createElement("div");
 const startBtn = document.getElementById("startBtn");
 const lightbox = document.getElementById("lightbox");
+const submitBtn = document.getElementById("submit");
 
 const player = (name, symbol) => {
-    return {name, symbol};
+  return { name, symbol };
+};
+
+const cell = (number) => {
+  
 };
 
 let cellArray = [];
 
 gridCell.classList.add("gridCell");
 
-for (let cellCount = 0; cellCount < 9; cellCount++){
-    let cell = cellArray[cellCount];
-    cell = gridCell.cloneNode(true);
-    gridContainer.appendChild(cell);
-    cell.addEventListener("click", cellClickFunc(cell));
-}
-
-function cellClickFunc (el) {
-    
+for (let cellCount = 0; cellCount < 9; cellCount++) {
+  let cell = gridCell.cloneNode(true);
+  cellArray[cellCount] = cell;
+  gridContainer.appendChild(cell);
+  cell.onclick = () => {
+    cell.innerText = "X";
+  }
 }
 
 startBtn.onclick = () => {
-    lightbox.className = "on";
+  lightbox.className = "on";
 }
 
-lightbox.onclick = () => {
-    lightbox.className = "off";
+submitBtn.addEventListener("click", submit);
+
+function submit() {
+  lightbox.className = "off";
 }
