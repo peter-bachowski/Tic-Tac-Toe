2bcf0323641a84e7fc5bb49a31792dfcb2954940
Peter <bachowskip@gmail.com>
2023-03-12 22:50:28 -0700

    Add functionality for the random AI, still need a function to check when it's a tie

--- script.js
+++ script.js
@@ -6,9 +6,11 @@ const gridContainer = document.getElementById("gridContainer");
 const gridCell = document.createElement("div");
 
 const startBtn = document.getElementById("startBtn");
-const submitBtn = document.getElementById("submit");
+const submitBtnOneVsOne = document.getElementById("submitBtnOneVsOne");
+const submitBtnVsAI = document.getElementById("submitBtnVsAI");
 const resetBtn = document.getElementById("resetBtn");
-const cancelBtn = document.getElementById("cancelBtn");
+const cancelBtnOneVsOne = document.getElementById("cancelBtnOneVsOne");
+const cencelBtnVsAI = document.getElementById("cancelBtnVsAI");
 
 const player1ScoreContainer = document.getElementById("player1ScoreContainer");
 const player2ScoreContainer = document.getElementById("player2ScoreContainer");
@@ -19,6 +21,7 @@ const lightbox = document.getElementById("lightbox");
 const formContainer = document.getElementById("formContainer");
 
 const playerInputsContainer = document.getElementById("playerInputsContainer");
+const submitContainerAI = document.getElementById("submitContainerAI");
 const option1 = document.getElementById("oneVsOne");
 const option2 = document.getElementById("vsAI");
 
@@ -37,14 +40,14 @@ let startPressed = false;
 let player1;
 let player2;
 let currentPlayer;
-
+let vsAiPressed; //boolean if playing vs AI or not
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 //functions
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 function createCells() { //creates the cells
-  removePlayerInputsContainer();
+  removeBothOptions();
   gridCell.classList.add("gridCell");
   for (let cellCount = 0; cellCount < 9; cellCount++) {
     const newCell = CellObj(cellCount);
@@ -56,18 +59,20 @@ function createCells() { //creates the cells
 }
 
 
-function submit(event) {
+function submitOneVsOne(event) {
   event.preventDefault();
 
-  let player1Name = document.getElementById("player1Name");
+  vsAiPressed = false;
+
+  let player1Name = document.getElementById("player1Name"); //gets the input elements from form
   let player2Name = document.getElementById("player2Name");
 
-  player1 = player(player1Name.value, "X");
+  player1 = player(player1Name.value, "X"); //asigns the names of each player from the form inputs
   player2 = player(player2Name.value, "O");
 
-  currentPlayer = player1.name;
+  currentPlayer = player1.name; //game starts current player with player 1
 
-  player1ScoreContainer.innerText = player1Name.value + ": X";
+  player1ScoreContainer.innerText = player1Name.value + ": X"; //shows which player has what symbol in the sidebar
   player2ScoreContainer.innerText = player2Name.value + ": O";
 
   startPressed = true;
@@ -75,9 +80,32 @@ function submit(event) {
 
   sidebarHeader.innerText = currentPlayer + " goes first!";
 
-  removePlayerInputsContainer();
+  removeBothOptions(); //removes both containers from form for next game
 
-  playOneVsOne();
+  playGame();
+}
+
+function submitVsAI(event) {
+  event.preventDefault();
+
+  vsAiPressed = true;
+
+  player1 = player("player 1", "X"); 
+  player2 = player("player 2", "O");
+
+  currentPlayer = player1.name;
+
+  player1ScoreContainer.innerText = "player 1: X"; //shows which player has what symbol in the sidebar
+  player2ScoreContainer.innerText = "AI: O";
+
+  startPressed = true;
+  lightbox.className = "off";
+
+  sidebarHeader.innerText = currentPlayer + " goes first!";
+
+  removeBothOptions(); //removes both containers from form for next game
+
+  playGame();
 }
 
 
@@ -96,32 +124,53 @@ function reset() {
   option1.checked = false;
   option2.checked = false;
 
-  removePlayerInputsContainer();
+  removeBothOptions();
 
   startPressed = false;
+  vsAiPressed = false;
 }
 
 
-function playOneVsOne() {
-  for (let i = 0; i < cellElements.length; i++) {
-    cellElements[i].onclick = () => {
-      if (startPressed === true) {
-        if (currentPlayer === player1.name) {
-          if (cellElements[i].innerText === "") {
-            cellElements[i].innerText = "X";
-            cellArray[i].symbol = "X"
-            currentPlayer = player2.name;
-            sidebarHeader.innerText = currentPlayer + "'s Turn!";
-            checkScores();
+function playGame() {
+
+  if (vsAiPressed === false) {
+    for (let i = 0; i < cellElements.length; i++) { //for every cell element add an onclick funtion
+      cellElements[i].onclick = () => {
+        if (startPressed === true) { //used so users can't press a cell unless start was pressed
+          if (currentPlayer === player1.name) { //if current player is player 1
+            if (cellElements[i].innerText === "") {
+              cellElements[i].innerText = "X"; //adds the player 1 symbol to the cell
+              cellArray[i].symbol = "X"; //adds the X symbol to the ojbect
+              currentPlayer = player2.name;
+              sidebarHeader.innerText = currentPlayer + "'s Turn!";
+              checkScores();
+            }
+          }
+          else {
+            if (cellElements[i].innerText === "") {
+              cellElements[i].innerText = "O";
+              cellArray[i].symbol = "O"
+              currentPlayer = player1.name;
+              sidebarHeader.innerText = currentPlayer + "'s Turn!";
+              checkScores();
+            }
           }
         }
-        else {
-          if (cellElements[i].innerText === "") {
-            cellElements[i].innerText = "O";
-            cellArray[i].symbol = "O"
-            currentPlayer = player1.name;
-            sidebarHeader.innerText = currentPlayer + "'s Turn!";
-            checkScores();
+      }
+    }
+  }
+  else if (vsAiPressed === true){
+    for (let i = 0; i < cellElements.length; i++) { //for every cell element add an onclick funtion
+      cellElements[i].onclick = () => {
+        if (startPressed === true) { //used so users can't press a cell unless start was pressed
+          if (currentPlayer === player1.name) { //if current player is player 1
+            if (cellElements[i].innerText === "") {
+              cellElements[i].innerText = "X"; //adds the player 1 symbol to the cell
+              cellArray[i].symbol = "X"; //adds the X symbol to the ojbect
+              sidebarHeader.innerText = currentPlayer + "'s Turn!";
+              checkScores();
+              recurseAiChoice();
+            }
           }
         }
       }
@@ -129,6 +178,21 @@ function playOneVsOne() {
   }
 }
 
+function recurseAiChoice() {
+  let randomCellNumber = Math.floor(Math.random() * 9);
+  if (cellElements[randomCellNumber].innerText === ""){
+    cellElements[randomCellNumber].innerText = "O";
+    cellArray[randomCellNumber].symbol = "O";
+  }
+  else {
+    recurseAiChoice();
+  }
+  checkScores();
+}
+
+function isTie() {
+
+}
 
 function checkScores() { //checks if there is three in a row of whichever symbol anywhere
   if (
@@ -144,6 +208,7 @@ function checkScores() { //checks if there is three in a row of whichever symbol
     sidebarHeader.innerText = player1.name + " is the Winner!"
     scoreHeader.innerText = "Game Over!"
     startPressed = false;
+    return true;
   }
 
   else if (
@@ -160,15 +225,21 @@ function checkScores() { //checks if there is three in a row of whichever symbol
     scoreHeader.innerText = "Game Over!";
 
     startPressed = false;
+    return true;
   }
 }
 
-function addPlayerInputsContainer() {
-  formContainer.appendChild(playerInputsContainer);
-}
+function addPlayerInputsContainer() {formContainer.appendChild(playerInputsContainer);}
 
-function removePlayerInputsContainer() {
-  playerInputsContainer.remove();
+function addSubmitContainerAI() {formContainer.appendChild(submitContainerAI);}
+
+function removePlayerInputsContainer() {playerInputsContainer.remove();}
+
+function removeSubmitContainerAI() {submitContainerAI.remove();}
+
+function removeBothOptions() {
+  removePlayerInputsContainer();
+  removeSubmitContainerAI();
 }
 
 
@@ -181,7 +252,7 @@ startBtn.onclick = () => {
   reset();
 }
 
-cancelBtn.onclick = () => {
+cancelBtnOneVsOne.onclick = () => {
   lightbox.className = "off";
 }
 
@@ -190,10 +261,18 @@ cancelBtn.onclick = () => {
 //listeners
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-submitBtn.addEventListener("click", submit);
+submitBtnOneVsOne.addEventListener("click", submitOneVsOne);
+submitBtnVsAI.addEventListener("click", submitVsAI);
 
-option1.addEventListener("click", addPlayerInputsContainer);
-option2.addEventListener("click", removePlayerInputsContainer);
+option1.addEventListener("click", () => {
+  removeBothOptions();
+  addPlayerInputsContainer();
+});
+
+option2.addEventListener("click", () => {
+  removeBothOptions();
+  addSubmitContainerAI();
+});
 
 resetBtn.addEventListener("click", reset);
 
